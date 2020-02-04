7aaebbbeb001ce7101115ec209ccfd73d8e82e1d
rehab <reremo45@gmail.com>
2020-02-04 16:33:23 +0200

    create request for books relates #5

--- js/request.js
+++ js/request.js
@@ -4,13 +4,35 @@ const searchResults = document.querySelector('.search-results');
 
 searchBtn.addEventListener('click', function() {
     const xhr = new XMLHttpRequest();
-    const url = `http://openlibrary.org/search.json?q=${searchQuery.value}`;
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${searchQuery.value}`;
     xhr.onreadystatechange = () => {
         if (xhr.readyState == 4 && xhr.status == 200) {
             console.log('ready state');
             const obj = JSON.parse(xhr.responseText);console.log(obj)
-            const link = obj.docs[0].title_suggest;console.log(link)
-            // searchResults.textContent = link;
+            const link = obj.items[0].volumeInfo.title;console.log(link);
+            const title = document.createElement('p');
+            title.textContent = link;
+            searchResults.appendChild(title);
+
+            const img = document.createElement('img');
+            img.src = obj.items[0].volumeInfo.imageLinks.thumbnail;
+            searchResults.appendChild(img);
+            
+            const authors = document.createElement('p');
+            authors.textContent = obj.items[0].volumeInfo.authors;
+            searchResults.appendChild(authors);
+
+            const publisher = document.createElement('p');
+            publisher.textContent = obj.items[0].volumeInfo.publisher;
+            searchResults.appendChild(publisher);
+            
+
+            const pageCount=document.createElement('p');
+            pageCount.textContent = obj.items[0].volumeInfo.pageCount;
+            searchResults.appendChild(pageCount);
+
+
+
         }
     };
     xhr.open('GET', url, true);
