27348f9e41c40bbec24fbf76a85691cffb7ee96e
rehab <reremo45@gmail.com>
2020-02-04 15:33:19 +0200

    create requist bokks relates #5

--- js/request.js
+++ js/request.js
@@ -1,11 +1,16 @@
+const searchQuery = document.querySelector('.search-query');
+const searchBtn = document.querySelector('.search-btn');
+const searchResults = document.querySelector('.search-results');
+
 searchBtn.addEventListener('click', function() {
     const xhr = new XMLHttpRequest();
-    const url = `http://openlibrary.org/search.json?q=`;
+    const url = `http://openlibrary.org/search.json?q=${searchQuery.value}`;
     xhr.onreadystatechange = () => {
         if (xhr.readyState == 4 && xhr.status == 200) {
-            const obj = JSON.parse(xhr.responseText);
-            const link = obj.docs;
-            
+            console.log('ready state');
+            const obj = JSON.parse(xhr.responseText);console.log(obj)
+            const link = obj.docs[0].title_suggest;console.log(link)
+            // searchResults.textContent = link;
         }
     };
     xhr.open('GET', url, true);
