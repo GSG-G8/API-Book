1c5475e22cb668f9b41d2b6fac66db39de997034
rehab <reremo45@gmail.com>
2020-02-05 14:20:32 +0200

    Edit style relates #17

--- js/request.js
+++ js/request.js
@@ -10,53 +10,59 @@ searchBtn.addEventListener('click', function() {
     xhr.onreadystatechange = () => {
         if (xhr.readyState == 4 && xhr.status == 200) {
             console.log('ready state');
-            const obj = JSON.parse(xhr.responseText);console.log(obj)
-            const link = obj.items[0].volumeInfo.title;console.log(link);
-            const title = document.createElement('p');
-            title.textContent = link;
-            searchResults.appendChild(title);
+            const obj = JSON.parse(xhr.responseText);
+
+            const item = document.createElement('div');
+            item.classList.add('item');
 
             const img = document.createElement('img');
             img.src = obj.items[0].volumeInfo.imageLinks.thumbnail;
-            searchResults.appendChild(img);
+            img.classList.add('cover');
+            item.appendChild(img);
+
+            const title = document.createElement('strong');
+            title.textContent = `Title: ${obj.items[0].volumeInfo.title}`;
+            item.appendChild(title);
             
             const authors = document.createElement('p');
-            authors.textContent = obj.items[0].volumeInfo.authors;
-            searchResults.appendChild(authors);
+            authors.textContent = `Authors: ${obj.items[0].volumeInfo.authors}`;
+            item.appendChild(authors);
 
             const publisher = document.createElement('p');
-            publisher.textContent = obj.items[0].volumeInfo.publisher;
-            searchResults.appendChild(publisher);
+            publisher.textContent = `Publisher: ${obj.items[0].volumeInfo.publisher}`;
+            item.appendChild(publisher);
             
 
             const pageCount=document.createElement('p');
-            pageCount.textContent = obj.items[0].volumeInfo.pageCount;
-            searchResults.appendChild(pageCount);
+            pageCount.textContent = `Page count: ${obj.items[0].volumeInfo.pageCount}`;
+            item.appendChild(pageCount);
 
             const categories=document.createElement('p');
-            categories.textContent = obj.items[0].volumeInfo.categories[0];
-            searchResults.appendChild(categories);
+            categories.textContent = `Category: ${obj.items[0].volumeInfo.categories[0]}`;
+            item.appendChild(categories);
 
             const language=document.createElement('p');
-            language.textContent = obj.items[0].volumeInfo.language;
-            searchResults.appendChild(language);
+            language.textContent = `Language: ${obj.items[0].volumeInfo.language}`;
+            item.appendChild(language);
 
             const description=document.createElement('p');
             description.textContent = obj.items[0].volumeInfo.description;
-            searchResults.appendChild(description);
+            description.classList.add('desc');
+            item.appendChild(description);
+
+            searchResults.appendChild(item);
         }}
             xhr.open('GET', url, true);
             xhr.send();
          
           
             const mykey = configObj.MY_KEY;
-            const urlMovies = `https://api.themoviedb.org/3/search/movie?api_key=${mykey}&query=${search-query.value}`;
+            const urlMovies = `https://api.themoviedb.org/3/search/movie?api_key=${mykey}&query=${searchQuery.value}`;
             const xhr1 = new XMLHttpRequest();
             xhr1.onreadystatechange = () => {
             if (xhr1.readyState === 4 && status === 200) {
             const movie = JSON.parse(xhr1.responseText);
-            show.textContent = movie.genres[0].name;console.log(movie)
-
+           
         }
     };
   
