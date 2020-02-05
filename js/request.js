c07e918f4a8e927f51ee4b680b2d18f374b3b9f5
rawan <rawan.designer@hotmail.com>
2020-02-05 10:30:30 +0200

    Adding new features to the request file Relates #5

--- js/request.js
+++ js/request.js
@@ -1,3 +1,4 @@
+const show = document.querySelector(".show-movie");
 const searchQuery = document.querySelector('.search-query');
 const searchBtn = document.querySelector('.search-btn');
 const searchResults = document.querySelector('.search-results');
@@ -5,6 +6,7 @@ const searchResults = document.querySelector('.search-results');
 searchBtn.addEventListener('click', function() {
     const xhr = new XMLHttpRequest();
     const url = `https://www.googleapis.com/books/v1/volumes?q=${searchQuery.value}`;
+    
     xhr.onreadystatechange = () => {
         if (xhr.readyState == 4 && xhr.status == 200) {
             console.log('ready state');
@@ -31,12 +33,11 @@ searchBtn.addEventListener('click', function() {
             pageCount.textContent = obj.items[0].volumeInfo.pageCount;
             searchResults.appendChild(pageCount);
 
-
             const categories=document.createElement('p');
             categories.textContent = obj.items[0].volumeInfo.categories[0];
             searchResults.appendChild(categories);
 
-          
+
             const language=document.createElement('p');
             language.textContent = obj.items[0].volumeInfo.language;
             searchResults.appendChild(language);
@@ -45,10 +46,22 @@ searchBtn.addEventListener('click', function() {
             description.textContent = obj.items[0].volumeInfo.description;
             searchResults.appendChild(description);
 
-
+        }
+    }
+    xhr.open('GET', url, true);
+    xhr.send();
+          
+            const mykey = configObj.MY_KEY;
+            const urlMovies = `https://api.themoviedb.org/3/search/movie?api_key=${mykey}&query=${search-query.value}`;
+            const xhr1 = new XMLHttpRequest();
+            xhr1.onreadystatechange = () => {
+            if (xhr1.readyState === 4 && status === 200) {
+            const movie = JSON.parse(xhr1.responseText);
+            show.textContent = movie.genres[0].name;console.log(movie)
 
         }
     };
-    xhr.open('GET', url, true);
-    xhr.send();
+  
+    xhr1.open("GET", urlMovies, true);
+    xhr1.send();
 });
